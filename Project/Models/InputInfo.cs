3dda5bed079ef71ef2f179a3fa49e66865507225
Minh Trang <minhtrang.9096@gmail.com>
2022-11-11 20:58:06 +0700

    done inputinfo

--- Project/Models/InputInfo.cs
+++ Project/Models/InputInfo.cs
@@ -1,21 +1,36 @@
-﻿using System;
+﻿using Project.ViewModel;
+using System;
 using System.Collections.Generic;
 
 namespace Project.Models
 {
-    public partial class InputInfo
+    public partial class InputInfo : BaseViewModel
     {
         
         public string Id { get; set; } = null!;
         public string IdObject { get; set; } = null!;
 
         public string IdInput { get; set; } = null!;
-        public int? Count { get; set; }
-        public double? InputPrice { get; set; }
-        public double? OutputPrice { get; set; }
-        public string? Status { get; set; }
 
-        public virtual Input IdInputNavigation { get; set; } = null!;
-        public virtual Object IdObjectNavigation { get; set; } = null!;
+        private int _Count;
+        public int? Count { get => _Count; set { _Count = (int)value; OnPropertyChanged(); } }
+
+        private double _InputPrice;
+        public double? InputPrice { get => _InputPrice; set { _InputPrice = (double)value; OnPropertyChanged(); } }
+
+        private double _OutputPrice;
+        public double? OutputPrice { get => _OutputPrice; set { _OutputPrice = (double)value; OnPropertyChanged(); } }
+
+        private string _Status;
+        public string? Status { get => _Status; set { _Status = value; OnPropertyChanged(); } }
+
+        //public virtual Input IdInputNavigation { get; set; } = null!;
+
+        private Input _IdInputNavigation;
+        public virtual Input IdInputNavigation { get => _IdInputNavigation; set { _IdInputNavigation = value; OnPropertyChanged(); } }
+        //public virtual Object IdObjectNavigation { get; set; } = null!;
+
+        private Object _IdObjectNavigation;
+        public virtual Object IdObjectNavigation { get => _IdObjectNavigation; set { _IdObjectNavigation = value; OnPropertyChanged(); } }
     }
 }
--- Project/ViewModel/InputViewModel.cs
+++ Project/ViewModel/InputViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Project.ViewModel
 {
@@ -12,6 +13,9 @@ namespace Project.ViewModel
     {
         private ObservableCollection<InputInfo> _List;
         public ObservableCollection<InputInfo> List { get => _List; set { _List = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<Input> _InputList;
+        public ObservableCollection<Input> InputList { get => _InputList; set { _InputList = value; OnPropertyChanged(); } }
 
         private InputInfo _SelectedItem;
         public InputInfo SelectedItem
@@ -28,12 +32,25 @@ namespace Project.ViewModel
                     InputPrice = SelectedItem.InputPrice;
                     OutputPrice = SelectedItem.OutputPrice;
                     Status = SelectedItem.Status;
-                    DateInput = SelectedItem.IdInputNavigation.DateInput;
+                    SelectedInput = SelectedItem.IdInputNavigation;
+                    DateInput = SelectedInput.DateInput;
 
                 }
             }
         }
-       
+
+        private Input _SelectedInput;
+        public Input SelectedInput
+        {
+            get => _SelectedInput;
+            set
+            {
+                _SelectedInput = value;
+                OnPropertyChanged();
+                DateInput = SelectedInput.DateInput;
+            }
+        }
+
 
         private string _ObjectDisplayName;
         public string? ObjectDisplayName { get => _ObjectDisplayName; set { _ObjectDisplayName = value; OnPropertyChanged(); } }
@@ -53,9 +70,69 @@ namespace Project.ViewModel
         private DateTime? _DateInput;
         public DateTime? DateInput { get => _DateInput; set { _DateInput = value; OnPropertyChanged(); } }
 
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
         public InputViewModel()
         {
             List = new ObservableCollection<InputInfo>(DataProvider.Ins.DB.InputInfos);
+            InputList = new ObservableCollection<Input>(DataProvider.Ins.DB.Inputs);
+
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                if (SelectedItem == null)
+                    return false;
+                return true;
+
+            }, (p) =>
+            {
+                var addObject = DataProvider.Ins.DB.Objects.Where(x => x.DisplayName == ObjectDisplayName).SingleOrDefault();
+                if (addObject != null )
+                {
+                    var Object = new InputInfo() { Id = Guid.NewGuid().ToString(), Count = Count, IdInput = SelectedInput.Id, IdObject = addObject.Id , InputPrice = InputPrice, OutputPrice = OutputPrice, Status = Status };
+
+                    DataProvider.Ins.DB.InputInfos.Add(Object);
+                    DataProvider.Ins.DB.SaveChanges();
+
+                    List.Add(Object);
+
+                    
+                }
+                
+            });
+
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (SelectedItem == null || SelectedInput == null)
+                    return false;
+
+                var displayList = DataProvider.Ins.DB.InputInfos.Where(x => x.Id == SelectedItem.Id);
+                if (displayList != null && displayList.Count() != 0)
+                    return true;
+
+                return false;
+
+            }, (p) =>
+            {
+                var Object = DataProvider.Ins.DB.InputInfos.Where(x => x.Id == SelectedItem.Id).SingleOrDefault();
+
+                var addObject = DataProvider.Ins.DB.Objects.Where(x => x.DisplayName == ObjectDisplayName).SingleOrDefault();
+                if (addObject != null)
+                {
+                    if (addObject.Id != SelectedItem.IdObject)
+                    {
+                        Object.IdObject = addObject.Id;
+                        Object.IdObjectNavigation = addObject;
+                    }
+                }
+
+                Object.Count = Count;
+                Object.InputPrice = InputPrice;
+                Object.OutputPrice = OutputPrice;
+                Object.Status = Status;
+                Object.IdInput = SelectedInput.Id;
+                Object.IdInputNavigation = SelectedInput;
+                DataProvider.Ins.DB.SaveChanges();
+            });
         }
     }
 }
